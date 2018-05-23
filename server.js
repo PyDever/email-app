8706fe3c342feeb6b7d4b3e65d27c0817b0b74b3
Kaleb Horvath <bobafett2021@hotmail.com>
2018-05-22 14:48:26 -1000

    huge update

--- server.js
+++ server.js
@@ -1,50 +1,54 @@
 
 // import express and the templating engine
-const express = require('express');
-const exphbs = require('express-handlebars');
+const express = require('express')
+const exphbs = require('express-handlebars')
 
 // import the http request library 
-const http = require('http');
+const http = require('http')
 
 // create a new instance of an application server
-const app = express();
+const app = express()
 
 // configure all the templating and public styles
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
-app.use(express.static('public'));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
 
-app.get ('/home', function (req, res) {
+// make express use the public folder 
+app.use(express.static('public'))
 
-    var queryString = req.query.term;
-    var term = encodeURIComponent(queryString);
+// write a handler for the index route
+app.get ('/', function (req, res) {
 
-    const remoteAddress = req.connection.remoteAddress;
+    var queryString = req.query.term
+    var term = encodeURIComponent(queryString)
+
+    // grab the remote address from the request object
+    const remoteAddress = req.connection.remoteAddress
 
     console.log(`[GET] Request from ${remoteAddress}`)
-    var url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=dc6zaTOxFJmzC';
+    var url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=dc6zaTOxFJmzC'
 
-
+    // make a get request to the giphy server 
     http.get(url, function (response) {
 
-        response.setEncoding('utf-8');
-
-        var body = ''; 
+        // set the response encoding to UTF-8
+        response.setEncoding('utf-8')
+        var body = ''
         response.on('data', function(d) {
-            body += d;
+            body += d
         });
+
          response.on('end', function(){
-            var parsed = JSON.parse(body);
+            var parsed = JSON.parse(body)
             res.render('home', {gifs: parsed.data})
          })
     })
 }) 
 
-app.get('/', function (req, res) {
-    return res.redirect('/home')
-})
-
+// listen in on port 3000 for incoming requests
 app.listen(3000,function(){
-  console.log("magic happens on port 3000...");
+
+    // alert the client that magic happens on port 3000
+    console.log("magic happens on port 3000...")
 });
 
